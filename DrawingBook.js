18eef58da05721dda5374fa45fca53ff9d9cce29
Laguna1 <lagunazoomarket@gmail.com>
2019-10-13 18:49:58 +0300

    add solution

---
+++ DrawingBook.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the pageCount function below.
+ */
+function pageCount(n, p) {
+    /*
+     * Write your code here.
+     */
+  /*
+          n: the number of pages in the book
+          p: the page number to turn to
+    */
+
+    const pageTurns = Math.floor(p / 2);
+    const totalTurns = Math.floor(n / 2);/* Returns the total number of page turns it takes to get
+    to a page or how many it requires if starting from the back */
+
+    return Math.min(pageTurns, totalTurns - pageTurns);
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const p = parseInt(readLine(), 10);
+
+    let result = pageCount(n, p);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
