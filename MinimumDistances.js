fe76cff2bd52ae2c07fd36d33300f39bafa4707c
Laguna1 <lagunazoomarket@gmail.com>
2019-10-15 12:51:58 +0300

    solution

---
+++ MinimumDistances.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumDistances function below.
+function minimumDistances(a) {
+let lastseen = [];
+let dist = -1;
+[...a].forEach((v, i) => {
+if (lastseen.hasOwnProperty(v)) {
+let thisdist = i - lastseen[v];
+dist = Math.min(thisdist, (dist === -1 ? thisdist : dist));
+}
+lastseen[v] = i;
+})
+return dist;
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    let result = minimumDistances(a);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
